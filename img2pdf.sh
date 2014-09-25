6ca155edd5e6dc6a9bb99813996d0bedc43d994c
Anish Shankar <rndanish@gmail.com>
2014-09-25 17:42:39 +0530

    Added support for 2images per page

--- img2pdf.sh
+++ img2pdf.sh
@@ -7,19 +7,21 @@
 SCALE=10
 BORDER_SIZE=30
 COMPRESS="JPEG" # or Lossless
+DOUBLEPERPAGE=false
 usage()
 {
 cat << EOF
-usage: $0 [-s scale] [-b border_percentage] [-l] IMAGES... OUTPUT_PDF
+usage: $0 [-s scale] [-b border_percentage] [-l] [-2] IMAGES... OUTPUT_PDF
 This script uses imagemagick to neatly generate a pdf out of a given set of images as arguments.
 OPTIONS:
     -s density in pixels per millimeter. Defaults to 10
     -b border size as a percentage. Defaults to 30%
     -l Use Lossless compression. (Default is jpeg)
+    -2 Use two images per page (Default is 1 image per page)
     -h This help message
 EOF
 }
-while getopts "hs:b:l" OPTION
+while getopts "hs:b:l2" OPTION
 do
   case $OPTION in
     h)
@@ -34,6 +36,9 @@ do
       ;;
     l)
       COMPRESS="Lossless"
+      ;;
+    2)
+      DOUBLEPERPAGE=true
       ;;
     ?)
       usage
@@ -60,6 +65,23 @@ echo "Writing Images to $OUTPUT_FILE" 1>&2
 IM_WD=$((210*SCALE))
 IM_HT=$((297*SCALE))
 DENSITY=$((SCALE*10))
-echo convert ${IMGFILES[@]} -bordercolor none -border ${BORDER_SIZE}%x${BORDER_SIZE}% -compress $COMPRESS -resize ${IM_WD}x${IM_HT} -gravity center -extent ${IM_WD}x${IM_HT} -units PixelsPerCentimeter -density ${DENSITY}x${DENSITY} $OUTPUT_FILE 1>&2
-convert ${IMGFILES[@]} -bordercolor none -border ${BORDER_SIZE}%x${BORDER_SIZE}% -compress $COMPRESS -resize ${IM_WD}x${IM_HT} -gravity center -extent ${IM_WD}x${IM_HT} -units PixelsPerCentimeter -density ${DENSITY}x${DENSITY} $OUTPUT_FILE
+
+COMMAND=""
+if [ "$DOUBLEPERPAGE" = true ]
+then
+  # TODO , The double image mechanism is not very nice at the moment.
+  # look into montage command for future.
+  HALFHT=$((IM_HT/2))
+  COMMAND="convert "
+  for i in $(seq 0 2 $((${#IMGFILES[@]}-1)))
+  do
+    COMMAND=$COMMAND" \( ${IMGFILES[$i]} ${IMGFILES[$((i+1))]} -gravity center -bordercolor none -border ${BORDER_SIZE}%x${BORDER_SIZE}% -resize ${IM_WD}x${HALFHT} -extent ${IM_WD}x${HALFHT} -append \) "
+  done
+  COMMAND=$COMMAND" -extent ${IM_WD}x${IM_HT} -units PixelsPerCentimeter -density ${DENSITY}x${DENSITY} -compress $COMPRESS $OUTPUT_FILE"
+else
+  COMMAND="convert ${IMGFILES[@]} -bordercolor none -border ${BORDER_SIZE}%x${BORDER_SIZE}% -compress $COMPRESS -resize ${IM_WD}x${IM_HT} -gravity center -extent ${IM_WD}x${IM_HT} -units PixelsPerCentimeter -density ${DENSITY}x${DENSITY} $OUTPUT_FILE"
+fi
+
+echo "$COMMAND"
+eval "$COMMAND"
 echo "converted to pdf of size $(du -sh $OUTPUT_FILE)"
